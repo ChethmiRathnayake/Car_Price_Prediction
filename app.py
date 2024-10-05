4d2cf7d52e7b1e5cf5da733c62bfe907755033a3
Chethmi Rathnayake <127948398+ChethmiRathnayake@users.noreply.github.com>
2024-10-06 02:11:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ st.title('Car Price Prediction App')
 st.header('Enter the vehicle details:')
 
 # Collecting the input features interactively from the user
-year = st.number_input('Year', min_value=1980, max_value=2024, value=2024)
-make = st.text_input('Make', '')
+year = st.number_input('Manufactured Year', min_value=1980, max_value=2024, value=2024)
+make = st.text_input('Brand', '')
 model = st.text_input('Model', '')
-trim = st.text_input('Trim', '')
+trim = st.text_input('Additional Designation(LX,T5)', '')
 body = st.selectbox('Body Type', ['Sedan', 'SUV', 'Truck', 'Coupe', 'Convertible', 'Wagon', 'Van'], index=0)
 state = st.text_input('State', '')
 condition = st.slider('Condition (1-100)', min_value=1, max_value=100, value=50)
